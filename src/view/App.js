45c81eae12739d4625eb31efc4257409d376d70a
ivana <jelisavcic.ivana@gmail.com>
2019-06-17 07:15:28 +0200

    feature_4

--- src/view/App.js
+++ src/view/App.js
@@ -1,40 +1,48 @@
-import React from 'react';
+import React from "react";
 import "../App.css";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
-import { UserList } from './Main';
-import { fetchUsers } from '../services/userService';
-import { Grid } from './ui/Grid';
-
-
+import { UserList } from "./Main";
+import { fetchUsers } from "../services/userService";
+import { Grid } from "./ui/Grid";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            users: []
-        }
-    }
+    this.state = {
+      users: []
+    };
+    this.onIconClick = this.onIconClick.bind(this);
+  }
 
-    componentDidMount() {
-        fetchUsers()
-            .then((data) => {
-                this.setState({ users: data });
-            })
-    }
+  onIconClick() {
+    this.setState(prevState => {
+        // localStorage.setItem("state", !prevState.listView)
+      return { listView: !prevState.listView };
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <Header title="React Users" />
-                <Grid users={this.state.users} />
-                {/* <UserList users={this.state.users} changeLayout="Grid" /> */}
-                <Footer />
-            </div>
-        );
-    }
+  componentDidMount() {
+    fetchUsers().then(data => {
+      this.setState({ users: data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header
+          onChangeLayout={this.onIconClick}
+          listView={this.state.listView}
+          title="React Users"
+        />
+        <Grid users={this.state.users} />
+        {/* <UserList users={this.state.users} changeLayout="Grid" /> */}
+        <Footer />
+      </div>
+    );
+  }
 }
 
-
-export default App
+export default App;
